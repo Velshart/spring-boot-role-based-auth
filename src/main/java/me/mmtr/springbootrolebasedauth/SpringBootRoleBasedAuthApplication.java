de8498074a75fff4446a026a34b0a0932ffc8f2e
Velshh <pkot2003@gmail.com>
2025-02-20 18:22:08 +0100

    Initial commit

---
+++ src/main/java/me/mmtr/springbootrolebasedauth/SpringBootRoleBasedAuthApplication.java
@@ -0,0 +1,13 @@
+package me.mmtr.springbootrolebasedauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRoleBasedAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRoleBasedAuthApplication.class, args);
+    }
+
+}
